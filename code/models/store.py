f95966f5f836eee9b095b0469ed79bbff7486a90
Aditya Sharma <adsharma@750-MAC-TJG5M.local>
2019-11-26 12:59:08 -0500

    fixed store model init method

--- code/models/store.py
+++ code/models/store.py
@@ -8,9 +8,8 @@ class StoreModel(db.Model):
 
     item = db.relationship('ItemModel', lazy='dynamic')
 
-    def __init__(self, name, price):
+    def __init__(self, name):
         self.name = name
-        self.price = price
 
     def json(self):
         return {"name":self.name, "items":[item.json() for item in self.items.all()]}
@@ -25,4 +24,4 @@ class StoreModel(db.Model):
 
     def delete_from_db(self):
         db.session.delete(self)
-        db.commit()
+        db.session.commit()
